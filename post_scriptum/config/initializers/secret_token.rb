081ce0190ceb4e2cc12d798a6ab017257b87825d
Michael Sogomonyan <ms.sogomonyan@gmail.com>
2013-11-21 21:22:27 +0600

    Initial commit

---
+++ post_scriptum/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PostScriptum::Application.config.secret_token = '53e1e6bbff2c3dbd67f318bdbaf6022947580b7f39643e3435bf6859b061a5348deb3356de234ddb9386be5499d1487f864d00445f770557044649fab650f771'
