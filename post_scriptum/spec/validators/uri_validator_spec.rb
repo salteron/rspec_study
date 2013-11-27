4b04d80c2e2e21a8d73c5a2114fa1c191a1732e7
Michael Sogomonyan <sogomonyan@ek.apress.ru>
2013-11-27 13:36:49 +0600

    Тестирует методы UriValidator'а

--- post_scriptum/spec/validators/uri_validator_spec.rb
+++ post_scriptum/spec/validators/uri_validator_spec.rb
@@ -22,8 +22,17 @@ describe UriValidator do
         UriValidator.any_instance.stub(:send_head_request).and_return(404)
       end
 
+      it 'should send head request exactly once' do
+        expect_any_instance_of(UriValidator).to receive(:send_head_request).
+          exactly(1).times.with(fake_model.url)
+
+         fake_model.valid?
+      end
+
       it { should_not be_valid }
+
       it { should have(1).errors_on(:url) }
+
 
       specify { fake_model.errors_on(:url).should include('is not available') }
     end
@@ -38,6 +47,24 @@ describe UriValidator do
       it { should have(1).errors_on(:url) }
 
       specify { fake_model.errors_on(:url).should include('is not available') }
+    end
+  end
+
+  describe 'valid_uri?' do
+    let(:uri_validator) { UriValidator.new(attributes: [:url]) }
+
+    it 'should be valid' do
+      uri = 'http://rubyonrails.org/images/rails.png'
+
+      expect(uri_validator.valid_uri?(uri)).to eq true
+    end
+
+    it 'should not be valid' do
+      uris = %w[a/b/c htt:/invalid.jpg http://www.ya.r#u#]
+
+      uris.each do |uri|
+         expect(uri_validator.valid_uri?(uri)).to eq false
+      end
     end
   end
 end
